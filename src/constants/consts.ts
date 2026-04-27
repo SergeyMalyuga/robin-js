a72d570fee50f901ff676bc56c7f534155795946
Sergey Malyuga <sergey@malyuga.ru>
2026-04-27 16:01:11 +0300

    feat(core): создать константы для data-атрибутов в компоненте main

---
+++ src/constants/consts.ts
@@ -0,0 +1,7 @@
+export const Hooks = {
+  HEADER: '[data-hook="header"]',
+  HERO: '[data-hook="hero"]',
+  POPULAR: '[data-hook="popular"]',
+  OFFERS: '[data-hook="offers"]',
+  NEWS: '[data-hook="news"]',
+} as const;
