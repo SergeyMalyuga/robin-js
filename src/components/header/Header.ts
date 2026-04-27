6cbe582abc001fca56a625c4f593a4fe5e61fedc
Sergey Malyuga <sergey@malyuga.ru>
2026-04-27 15:59:50 +0300

    feat(header): создать компонент

---
+++ src/components/header/Header.ts
@@ -0,0 +1,39 @@
+import styles from './Header.module.scss';
+
+export class Header {
+  public render() {
+    return `
+    <header class="${styles.header}" role="banner">
+    <div class="${styles.content} container">
+    <a class="${styles.linkLogo}" href="/">
+<img src="/images/vector/logo.svg" width="78" height="24" alt="Логотип компании"/>
+</a>
+<nav aria-label="Основная навигация по сайту">
+<ul class="${styles.navLinks}">
+<li>
+<a href="#">
+Experience
+</a>
+</li>
+<li>
+<a href="#">
+Work
+</a>
+</li>
+<li>
+<a href="#">
+Photography
+</a>
+</li>
+<li>
+<a href="#">
+Contact
+</a>
+</li>
+</ul>
+</nav>
+</div>
+</header>
+`
+  }
+}
