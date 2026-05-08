c08b5d9352900760eb2f16961091cd2c98fdda6f
Sergey Malyuga <sergey@malyuga.ru>
2026-05-08 13:33:41 +0300

    feat(main): реализовать удаление всех слушателей при перезагрузки страницы

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import './styles.scss';
-import { MainPage } from './pages/MainPage.ts';
-import { Header } from './components/header/Header.ts';
+import {MainPage} from './pages/MainPage.ts';
+import {Header} from './components/header/Header.ts';
 
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('app') as HTMLElement;
   const header = new Header();
-  const mainPage = new MainPage(container, { header });
+  const mainPage = new MainPage(container, {header});
   mainPage.render();
+
+  window.addEventListener('beforeunload', () => {
+    mainPage.destroy();
+  })
 });
