3faf22169c76c5ac3906d077d483320e0493ae3d
Sergey Malyuga <sergey@malyuga.ru>
2026-05-13 15:42:08 +0300

    feat(home-page): подключить секцию Hero

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import './styles.scss';
 import {MainPage} from './pages/MainPage.ts';
 import {Header} from './components/header/Header.ts';
+import {Hero} from './components/hero/Hero.ts';
 
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('app') as HTMLElement;
   const header = new Header();
-  const mainPage = new MainPage(container, {header});
+  const hero = new Hero();
+  const mainPage = new MainPage(container, {header, hero});
   mainPage.render();
 
   window.addEventListener('beforeunload', () => {
--- src/pages/MainPage.ts
+++ src/pages/MainPage.ts
@@ -1,20 +1,25 @@
 import type {DataHook} from '../types/DataHook.type.ts';
 import type {MainProps} from '../models/MainProps.ts';
 import type {Header} from '../components/header/Header.ts';
+import type {Hero} from '../components/hero/Hero.ts';
 
 export class MainPage {
-  private container!: HTMLElement;
-  private header!: Header;
+  private container: HTMLElement;
+  private header: Header;
+  private hero: Hero;
 
   public constructor(container: HTMLElement, props: MainProps) {
     this.container = container;
     this.header = props.header;
+    this.hero = props.hero;
   }
 
   public render(): void {
     this.container.innerHTML = `
 ${this.header.render()}
-    <main></main>
+    <main>
+    ${this.hero.render()}
+</main>
     `;
     this.header.init();
   }
