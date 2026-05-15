39ee5b402e60f5419d105aa31cd38404b06d97e1
Sergey Malyuga <sergey@malyuga.ru>
2026-05-15 15:15:42 +0300

    refactor(Hero): изменить обёртку изображения

--- src/components/hero/Hero.ts
+++ src/components/hero/Hero.ts
@@ -6,9 +6,10 @@ export class Hero {
     return `
   <section class="${styles.hero}">
   <div class="${styles.content}">
-  <div class="${styles.wrapperImage}">
+  <picture class="${styles.wrapperImage}">
+  <source srcset="/images/raster/hero-robin-mobile.jpg" media="(max-width: 360px)">
 <img class="${styles.image}" src="/images/raster/hero-robin.jpg" width="750" height="650" alt="Robin W." loading="eager"/>
-</div>
+</picture>
 <div class="${styles.info}">
 <h1 class="${styles.title}">I’m Robin Williams. A Product Designer <span>based in Italy.</span></h1>
 <p >I’m probably the most passionate designer you will ever get to work with. If you have a great project that needs some amazing skills, I’m your guy.</p>
