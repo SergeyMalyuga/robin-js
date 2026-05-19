dc43ff3a90d132f23c9b8a123f999cfc3bcbda4c
Sergey Malyuga <sergey@malyuga.ru>
2026-05-19 13:40:09 +0300

    style(Experience): адаптировать padding под мобильные устройства

--- src/components/experience/Experience.ts
+++ src/components/experience/Experience.ts
@@ -3,7 +3,7 @@ import styles from './Experience.module.scss';
 export class Experience {
   public render() {
     return `
-<section class="${styles.experience}">
+<section class="${styles.experience} section">
 <div class="container">
      <h2 class="section-title section-title--small ${styles.titleMain}">work experience</h2>
    <p class="section-subtitle ${styles.subtitle}">Companies I have worked for in the past</p>
