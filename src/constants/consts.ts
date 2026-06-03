f4ea9f3e38f808e11d73646271d3e496032f054e
Sergey Malyuga <sergey@malyuga.ru>
2026-06-03 11:13:02 +0300

    feat(consts): добавить константные данные карточек путешествий

--- src/constants/consts.ts
+++ src/constants/consts.ts
@@ -1,7 +1,98 @@
+import type {CityCard} from '../models/CityCard.ts';
+
 export const Hooks = {
   EXPERIENCE: '[data-hook="experience"]',
-  HERO: '[data-hook="hero"]',
+  GALLERY: '[data-hook="gallery"]',
   POPULAR: '[data-hook="popular"]',
   OFFERS: '[data-hook="offers"]',
   NEWS: '[data-hook="news"]',
 } as const;
+
+export enum City {
+  ITALY = 'italy',
+  AUSTRALIA = 'australia',
+  INDIA = 'india',
+  BRAZIL = 'brazil'
+}
+
+export const CITY_LOCATION: CityCard = {
+  [City.ITALY]: [
+    {
+    url: '/images/raster/italy-street.jpg',
+    alt: 'Street',
+    width: 306,
+    height: 420,
+  },
+    {
+      url: '/images/raster/italy-cafe.jpg',
+      alt: 'Cafe',
+      width: 306,
+      height: 420,
+    },
+    {
+      url: '/images/raster/italy-chanal.jpg',
+      alt: 'Chanal',
+      width: 306,
+      height: 420,
+    },
+    {
+      url: '/images/raster/italy-sea.jpg',
+      alt: 'Sea',
+      width: 306,
+      height: 420,
+    }
+  ],
+  [City.AUSTRALIA]: [
+    {
+      url: '/images/raster/australia-lake.jpg',
+      alt: 'Lake',
+      width: 306,
+      height: 420,
+    },
+    {
+      url: '/images/raster/australia-kangaroo.jpg',
+      alt: 'Kangaroo',
+      width: 306,
+      height: 420,
+    },
+    {
+      url: '/images/raster/australia-bridge.jpg',
+      alt: 'Bridge',
+      width: 306,
+      height: 420,
+    },
+    {
+      url: '/images/raster/australia-mountains.jpg',
+      alt: 'Mountains',
+      width: 306,
+      height: 420,
+    },
+    {
+      url: '/images/raster/australia-sea.jpg',
+      alt: 'Sea',
+      width: 306,
+      height: 420,
+    },
+  ],
+  [City.INDIA]: [],
+  [City.BRAZIL]: [
+    {
+      url: '/images/raster/brazil-carnival.avif',
+      alt: 'Carnival',
+      width: 306,
+      height: 420,
+    },
+    {
+      url: '/images/raster/brazil-river.avif',
+      alt: 'River',
+      width: 306,
+      height: 420,
+    },
+    {
+      url: '/images/raster/brazil-monument.jpg',
+      alt: 'Monument',
+      width: 306,
+      height: 420,
+    }
+  ],
+}
