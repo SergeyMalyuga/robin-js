99488f3ac235ed59df3a95cd52d4b7cd76ac7f78
Sergey Malyuga <sergey@malyuga.ru>
2026-06-03 15:29:38 +0300

    feat(MainProps): добавить свойство footer в модель главной страницы

--- src/models/MainProps.ts
+++ src/models/MainProps.ts
@@ -9,6 +9,7 @@ import type {Dribbble} from '../components/dribbble/Dribbble.ts';
 import type {Response} from '../components/response/Response.ts';
 import type {Gallery} from '../components/gallery/Gallery.ts';
 import type {Contact} from '../components/contact/Contact.ts';
+import type {Footer} from '../components/footer/Footer.ts';
 
 export interface MainProps {
   header: Header;
@@ -22,4 +23,5 @@ export interface MainProps {
   response: Response;
   gallery: Gallery;
   contact: Contact;
+  footer: Footer;
 }
