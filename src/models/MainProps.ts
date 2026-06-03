2753ad572bb3c7aea8ac0a044351348091ad44b6
Sergey Malyuga <sergey@malyuga.ru>
2026-06-03 11:12:25 +0300

    feat(mainProps): добавить свойство gallery в модель главной страницы

--- src/models/MainProps.ts
+++ src/models/MainProps.ts
@@ -7,6 +7,7 @@ import type {Projects} from '../components/projects/Projects.ts';
 import type {Instagram} from '../components/instagram/Instagram.ts';
 import type {Dribbble} from '../components/dribbble/Dribbble.ts';
 import type {Response} from '../components/response/Response.ts';
+import type {Gallery} from '../components/gallery/Gallery.ts';
 
 export interface MainProps {
   header: Header;
@@ -18,4 +19,5 @@ export interface MainProps {
   instagram: Instagram;
   dribbble: Dribbble;
   response: Response;
+  gallery: Gallery;
 }
