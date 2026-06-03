2c0c97e73792694e33997aa20f4f53503820776d
Sergey Malyuga <sergey@malyuga.ru>
2026-06-03 11:11:44 +0300

    feat(CityCard): создать модель карточки города

--- src/models/CityCard.ts
+++ src/models/CityCard.ts
@@ -1,4 +1,4 @@
 import type {City} from '../constants/consts.ts';
-import type {Image} from '../types/Image.ts';
+import type {Image} from './Image.ts';
 
 export type CityCard = Record<City, Image[]>;
